010cd47f6b16c3231a2ae9b2cb9162149e684288
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-13 19:00:02 +0100

    add Pizza model class and PizzaController class

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/models/Pizza.java
@@ -0,0 +1,31 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.models;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    private String image;
+
+    @NotNull
+    private BigDecimal price;
+
+}
