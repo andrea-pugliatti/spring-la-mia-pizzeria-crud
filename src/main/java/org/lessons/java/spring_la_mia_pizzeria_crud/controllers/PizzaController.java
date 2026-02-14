b8a5405fcac291639fcb708a6fbf09115216939f
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-14 17:27:50 +0100

    add query and controller method for search

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -37,4 +38,12 @@ public class PizzaController {
         model.addAttribute("pizza", pizza.get());
         return "pizzas/show";
     }
+
+    @GetMapping("/search")
+    public String getIndex(Model model, @RequestParam("q") String name) {
+        List<Pizza> list = repo.findByNameContaining(name);
+        model.addAttribute("pizzas", list);
+        return "pizzas/index";
+    }
+
 }
--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repositories/PizzaRepository.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repositories/PizzaRepository.java
@@ -1,8 +1,11 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.repositories;
 
+import java.util.List;
+
 import org.lessons.java.spring_la_mia_pizzeria_crud.models.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
+    public List<Pizza> findByNameContaining(String name);
 }
