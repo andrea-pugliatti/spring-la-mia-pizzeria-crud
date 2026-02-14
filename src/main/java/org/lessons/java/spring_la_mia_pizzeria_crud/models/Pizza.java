b24c7d850f8890de30f7ff266ec0a483e8f9142f
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-14 10:40:14 +0100

    add getters and setters to Pizza class

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/models/Pizza.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/models/Pizza.java
@@ -30,4 +30,49 @@ public class Pizza {
     @NotNull
     private BigDecimal price;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+
 }
